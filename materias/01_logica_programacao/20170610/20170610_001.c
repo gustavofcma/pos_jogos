51ffd6bbed763d21b1b450f8cab7e009b63f821e
GUSTAVO ALMEIDA <2017000665@UC.ACAD>
2017-06-10 08:29:35 -0300

    Teste

---
+++ materias/01_logica_programacao/20170610/20170610_001.c
@@ -0,0 +1,12 @@
+// ARQUIVO COPIADO DO AVA
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main() {
+    int cont;
+    for(cont=0;cont<3;cont++) {
+            printf("Testando a repeticao \n");
+    }
+    system("pause");
+}
