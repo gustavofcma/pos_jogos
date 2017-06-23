68f63dd86a59d565e8a77da826cb0b49776067e2
Gustavo de Almeida <gustavofcma@gmail.com>
2017-06-23 19:04:08 -0300

    finalizando o exercício

--- materias/01_logica_programacao/20170610/20170610_008.c
+++ materias/01_logica_programacao/20170610/20170610_008.c
@@ -1,25 +1,30 @@
 // EXERCÍCIO DO SLIDE 10
+// RECEBER IDADE DE 5 PESSOAS E EXIBIR A MAIS VELHA E A MAIS NOVA
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <locale.h>
+#include <locale.h> //biblioteca para utilizar a função setlocale que permite utilização de acentos e notação decimal com vírgulas
+#include <limits.h> //biblioteca para utilizar a constante INT_MAX que indica o maior valor possível para um valor int
 
 main() {
-    int i, idade, pessoas = 5;
+    int i, idade, maior_idade=-1, menor_idade=INT_MAX, maior_pos, menor_pos, pessoas = 5;
 
     setlocale(LC_ALL, "");
 
     printf("-----------------------------------------\n");
     for (i=0; i<pessoas; i++) {
         printf(" Digite a idade da %dª pessoa: ", i+1);
-        scanf(" %f",&idade);
-        if (toupper(sexo) == 'F') {
-            mulheres++;
-            soma += idade;
+        scanf(" %d",&idade);
+        if (idade > maior_idade) {
+            maior_idade = idade;
+            maior_pos = i+1;
+        }
+        if (idade < menor_idade) {
+            menor_idade = idade;
+            menor_pos = i+1;
         }
         printf("-----------------------------------------\n");
     }
-    printf("\nTotal de mulheres: %d", mulheres);
-    printf("\nMédia de idade das mulheres: %.2f\n", soma/mulheres);
-
+    printf("\nA %dª pessoa é a mais nova, com %d anos!", menor_pos, menor_idade);
+    printf("\nA %dª pessoa é a mais velha, com %d anos!\n", maior_pos, maior_idade);
 }
